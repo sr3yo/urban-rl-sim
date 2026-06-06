795c3f5f46864d94053b6d6595e9f311e23362de
sr3yo <sawsib560@gmail.com>
2026-06-06 15:44:19 -0400

    reset function implemented

--- environment.py
+++ environment.py
@@ -30,4 +30,48 @@ class CarEnvironment(gym.Env):
         self.observation_space = gym.spaces.Box(
             low = 0, high = 200, shape = (5,), dtype=np.float32
         )
+
+
+        #initialize pygame attributes
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        #drawing track 
+        self.track_surface = pygame.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.track_surface.fill((124, 252, 0))
+        pygame.draw.ellipse(self.track_surface, (80,80,80), (20,20,960,500), 80)
         
+
+
+        #function to cast array
+        def cast_ray(x, y, angle):
+            for distance in range(0, 200):
+                #tip of ray each time
+                ray_x = x + distance * math.cos(math.radians(angle))
+                ray_y = y - distance * math.sin(math.radians(angle))
+
+                #check off screen bounds
+                if ray_x >= self.SCREEN_WIDTH or ray_y >= self.SCREEN_HEIGHT or ray_x < 0 or ray_y < 0:
+                    return distance
+                color = self.track_surface.get_at((int(ray_x), int(ray_y)))
+                if color[:3] != self.ROAD_COLOR:
+                    return distance
+            return 200
+
+
+        #reset function
+        def reset(self):
+            self.x = self.start_x
+            self.y = self.start_y
+            self.angle = self.start_angle
+            self.speed = 0
+
+            ray_angles = [self.angle, self.angle+45, self.angle-45, self.angle+90, self.angle-90]
+            distances = []
+
+            for ray_angle in ray_angles:
+                d = self.cast_ray(self.x, self.y, ray_angle)
+                distances.append(d)
+            
+            return np.array(distances, dtype=np.float32), {}
